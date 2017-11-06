ba9da611592a41fbaf0f25dbb5adb90d1b6c6922
vovanquang12cntt <vanquang451@gmail.com>
2017-11-06 13:39:31 +0700

    add file foot.php,head.php and newproduct.php, include files over to index.php

---
+++ foot.php
@@ -0,0 +1,47 @@
+<div class="footer">
+		<div class="container">
+			<div class="info">
+				<div class="row">
+					<div class="col-md-3 col-xs-12 col-sm-6 colu">
+						<p>quick links</p>
+						<ul>
+							<li><a href="#" id="">home</a></li>
+							<li><a href="#" id="">new products</a></li>
+							<li><a href="#" id="">all products</a></li>
+							<li><a href="#" id="">specials</a></li>
+							<li><a href="#" id="">contacts</a></li>
+							<li><a href="#" id="">reviews</a></li>
+						</ul>
+					</div>
+					<div class="col-md-3 col-xs-12 col-sm-6 colu">
+						<p>customers</p>
+						<ul>
+							<li><a href="#" id="">login</a></li>
+							<li><a href="#" id="">create acount</a></li>
+							<li><a href="#" id="">reviews</a></li>
+							<li><a href="#" id="">ships & return</a></li>
+						</ul>
+					</div>
+					<div class="col-md-3 col-xs-12 col-sm-6 colu">
+						<p>flolow us</p>
+						<ul>
+							<li><a href="#" id="">fabook</a></li>
+							<li><a href="#" id="">twiter</a></li>
+							<li><a href="#" id="">rss</a></li>
+						</ul>	
+					</div>
+					<div class="col-md-3 col-xs-12 col-sm-6 colu">
+						<p>contact us</p>
+						<ul>
+							<li><i class="fa fa-phone-square" aria-hidden="true"></i> 098765431</li>
+							<li><i class="fa fa-envelope" aria-hidden="true"></i> frteam@gmail.com</li>
+						</ul>	
+					</div>
+				</div>
+			</div>
+			<hr>
+			<div class="rules">
+				<p>Copyright&copy; Frteam | MNM | 2017</p>
+			</div>
+		</div>
+	</div>
---
+++ head.php
@@ -0,0 +1,35 @@
+<div class="head">
+	<div class="menu">
+		<div class="container">
+			<div class="menu_left">
+				<ul>
+					<li><a href="#" id="home">home</a></li>
+					<li><a href="#" id="new">new products</a></li>
+					<li><a href="#" id="all">all products</a></li>
+					<li><a href="#" id="specials">specials</a></li>
+					<li><a href="#" id="contact">contacts</a></li>
+					<li><a href="#" id="review">reviews</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="banner">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-4 col-md-4">
+					<img id="img_moto" src="images/iconmoto2.png" alt="" width="70" height="60">
+					<h3>MOTORCYCLES</h3>
+				</div>
+				<div class="col-xs-12 col-sm-4 col-md-4">
+					<form action="" method="POST" class="form-inline" role="form">
+						<div class="form-group">
+							<input type="text" class="input_css" id="" placeholder="search">
+						</div>
+						<button type="submit" class="butn_search"><i class="fa fa-search" aria-hidden="true"></i></button>
+					</form>
+				</div>
+				<div class="col-xs-12 col-sm-4 col-md-4"></div>
+			</div>
+		</div>
+	</div>
+</div>	
