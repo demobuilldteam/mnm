eb29a4f67ebe04571a5a8c35a730648fb5e8cb89
vovanquang12cntt <vanquang451@gmail.com>
2017-11-06 18:41:06 +0700

    create files allproduct.php,contact.php menu_left_content.php special.php

---
+++ menu_left_content.php
@@ -0,0 +1,97 @@
+<div class="menu_left_content">
+				<div class="menu_left_top">
+					<!-- 1 -->
+					<div class="menu_left_level1">
+						<div class="title">
+							<p>helmets</p>
+						</div>
+
+						<i class="fa fa-angle-double-right fa1" aria-hidden="true"></i>
+					</div>
+					<ul class="menu_sub sub1">
+							<li><a href="#" id="">APPAREL</a></li>
+							<li><a href="#" id="">BRAKES</a></li>
+							<li><a href="#" id="">HANDLING</a></li>
+							<li><a href="#" id="">ENGINE</a></li>
+					</ul>
+
+
+					<!-- 2 -->
+					<div class="menu_left_level1">
+						<div class="title">
+							<p>mem's gear</p>
+						</div>
+						<i class="fa fa-angle-double-right fa2" aria-hidden="true"></i>
+					</div>
+					<ul class="menu_sub sub2">
+							<li><a href="#" id="">APPAREL</a></li>
+							<li><a href="#" id="">BRAKES</a></li>
+							<li><a href="#" id="">HANDLING</a></li>
+							<li><a href="#" id="">ENGINE</a></li>
+					</ul>
+
+					<!-- 3 -->
+					<div class="menu_left_level1">
+						<div class="title">
+							<p>parts</p>
+						</div>
+						
+						<i class="fa fa-angle-double-right fa3" aria-hidden="true"></i>
+					</div>
+					<ul class="menu_sub sub3">
+							<li><a href="#" id="">APPAREL</a></li>
+							<li><a href="#" id="">BRAKES</a></li>
+							<li><a href="#" id="">HANDLING</a></li>
+							<li><a href="#" id="">ENGINE</a></li>
+					</ul>
+					<!-- 4 -->
+					<div class="menu_left_level1">
+						<div class="title">
+							<p>parts</p>
+						</div>
+						
+						
+					</div>
+					<!-- 5 -->
+					<div class="menu_left_level1">
+						<div class="title">
+							<p>parts</p>
+						</div>
+						
+						
+					</div>
+					<!-- 6 -->
+					<div class="menu_left_level1">
+						<div class="title">
+							<p>parts</p>
+						</div>
+						
+						
+					</div>
+					<!-- 7 -->
+					<div class="menu_left_level1">
+						<div class="title">
+							<p>parts</p>
+						</div>
+						
+						
+					</div>
+				</div>
+				<div class="menu_left_bottom">
+					<h4>specials</h4>
+					<div class="item_product">
+						<img src="images/iconmoto.png" alt="" width="100" height="50">
+						<div class="item_content">
+							<p>Produc's Name</p>
+							<p><strong>Price</strong></p>
+						</div>
+					</div>
+					<div class="item_product">
+						<img src="images/iconmoto.png" alt="" width="100" height="50">
+						<div class="item_content">
+							<p>Produc's Name</p>
+							<p><strong>Price</strong></p>
+						</div>
+					</div>
+				</div>
+			</div>
