00df396faf8cc06faa60b86fa217d76ef1bd51f2
vovanquang12cntt <vanquang451@gmail.com>
2017-11-28 22:15:31 +0700

    update js

--- js/wethear.js
+++ js/wethear.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$('#show_wethear').click(function(event) {
 		/* Act on the event */
 		var city = $('#city').val();
-		if(city != ' '){
+		if(city != ''){
 			$.$.ajax({
 				url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city+ '&&appid=b1b15e88fa797225412429c1c50c122a1',
 				type: 'GET',
@@ -10,17 +10,7 @@ $(document).ready(function() {
 				success: function(data){
 					console.log(data);
 				}
-			})
-			.done(function() {
-				console.log("success");
-			})
-			.fail(function() {
-				console.log("error");
-			})
-			.always(function() {
-				console.log("complete");
 			});
-			
 		}else{
 			$('#error_city').innerHTML = "You must enter city's name";
 		}
