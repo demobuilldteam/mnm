5937d3dabce7385256d5c9c3d72269a49f405d10
vovanquang12cntt <vanquang451@gmail.com>
2017-11-30 18:06:32 +0700

    update api map

--- index.php
+++ index.php
@@ -256,7 +256,7 @@
 		}
 	</script>
 
-  <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD-PVEwxvxIPSiiHzvGt0isp460L9qH1Ro=moto-shop"
+  <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD-PVEwxvxIPSiiHzvGt0isp460L9qH1Ro&callback=moto-shop"
   type="text/javascript"></script>
 </body>
 </html>
