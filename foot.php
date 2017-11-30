91f88ec83cb295be131f23fd73b87274fcbc27e6
vovanquang12cntt <vanquang451@gmail.com>
2017-11-30 17:42:24 +0700

    add api map

--- foot.php
+++ foot.php
@@ -1,5 +1,8 @@
 <div class="footer">
 	<div class="container">
+		<div id="map" style="width:100%;height:500px"></div>
+			
+		</div>
 		<div class="info">
 			<div class="row">
 				<div class="col-md-3 col-xs-12 col-sm-6 colu">
@@ -45,3 +48,21 @@
 		</div>
 	</div>
 </div>
+<script>
+function myMap() {
+  var myCenter = new google.maps.LatLng(16.054407,108.202167);
+  var mapCanvas = document.getElementById("map");
+  var mapOptions = {center: myCenter, zoom: 5};
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+  var marker = new google.maps.Marker({position:myCenter});
+  marker.setMap(map);
+
+  // Zoom to 9 when clicking on marker
+  google.maps.event.addListener(marker,'click',function() {
+    map.setZoom(9);
+    map.setCenter(marker.getPosition());
+  });
+}
+</script>
+
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBu-916DdpKAjTmJNIgngS6HL_kDIKU0aU&callback=myMap"></script>
