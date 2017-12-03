75c53d31d4684a71753a5569d8ab8f7ec7247fe0
vovanquang12cntt <vanquang451@gmail.com>
2017-12-03 19:33:08 +0700

    finished 96%

--- connect.php
+++ connect.php
@@ -1,23 +1,23 @@
 <?php
-	$username = "root";//sql12207584
-	$password = "vovanquang";//KzyYpsTKu2
-	$servername ="localhost";//sql12.freemysqlhosting.net
+	// $username = "root";//sql12207584
+	// $password = "vovanquang";//KzyYpsTKu2
+	// $servername ="localhost";//sql12.freemysqlhosting.net
 
-	$dbname = "shopmotor";//sql12207584
-	$conn = mysqli_connect($servername,$username,$password,$dbname);
-	mysqli_set_charset($conn, 'utf8');
-	if(!$conn){
-		die("không thể kết nối..".mysqli_connect_error());
-	}
-	// $username = "sql12207584";
-	// $password = "KzyYpsTKu2";
-	// $servername ="sql12.freemysqlhosting.net";
-
-	// $dbname = "sql12207584";
+	// $dbname = "shopmotor";//sql12207584
 	// $conn = mysqli_connect($servername,$username,$password,$dbname);
 	// mysqli_set_charset($conn, 'utf8');
 	// if(!$conn){
 	// 	die("không thể kết nối..".mysqli_connect_error());
 	// }
+	$username = "sql12207584";
+	$password = "KzyYpsTKu2";
+	$servername ="sql12.freemysqlhosting.net";
+
+	$dbname = "sql12207584";
+	$conn = mysqli_connect($servername,$username,$password,$dbname);
+	mysqli_set_charset($conn, 'utf8');
+	if(!$conn){
+		die("không thể kết nối..".mysqli_connect_error());
+	}
 	
 ?>
